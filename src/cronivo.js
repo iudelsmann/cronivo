d1d64605a44b8fa1e034d6023b8b3ecc821366e2
Ivo Udelsmann <iudelsmann@gmail.com>
2016-11-28 22:21:54 -0200

    Multiple fixes
    - Replace moment for javascript date (less depedencies)
    - Set milliseconds to zero on nextExecution since later does not set it
    - Save redisClient for set method

--- src/cronivo.js
+++ src/cronivo.js
@@ -3,6 +3,7 @@
 const _ = require('lodash');
 const later = require('later');
 
+let redisClient;
 let redisLock;
 
 function setInterval(func, schedule, jobName) {
@@ -15,10 +16,12 @@ function setInterval(func, schedule, jobName) {
       redisClient.get(jobName, (err, reply) => {
         // If the next execution is null or greater then the current next, then this job must
         // execute, else, it was already executed
-        const nextExecution = moment(schedule.next(1)[0]).valueOf();
+        const nextExecution = later.schedule(schedule).next(1);
+        // Sets milliseconds to zero since later doesn't use or set it
+        nextExecution.setMilliseconds(0);
         if(_.isNil(reply) || reply.toString() < nextExecution) {
           // Sets the next execution time on redis so other jobs wont run
-          redisClient.set(jobName, nextExecution, () => {
+          redisClient.set(jobName, nextExecution.getTime(), () => {
             // Releases the lock since it is no longer required
             done();
             // Calls the method passed by the user
@@ -32,7 +35,8 @@ function setInterval(func, schedule, jobName) {
   }, schedule);
 }
 
-function setUp(redisClient) {
+function setUp(client) {
+  redisClient = client;
   // Uses the provided client to configure redisLock
   redisLock = require('redis-lock')(redisClient);
   // Returns available methods

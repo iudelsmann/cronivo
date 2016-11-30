f6fcbb17611266eb638d0dc0a14dfccd81a263ae
Ivo Udelsmann <ivo.udelsmann@evoluservices.com>
2016-11-30 11:34:02 -0200

    More unit tests
    - Add tests for setInterval method
    - Refactor setInterval to use current time to verify if job must execute (current time > nextExecution)

--- spec/tests/cronivo.test.js
+++ spec/tests/cronivo.test.js
@@ -1,17 +1,39 @@
 'use strict';
 
 const proxyquire = require('proxyquire').noCallThru();
+const later = require('later');
 
 describe('cronivo:', () => {
-  let cronivo;
+  const redisClient = {};
 
+  let cronivo;
   let redisLock;
+  let redisLockMethod;
+  let freeLock;
+
+  const basetime = new Date('2000-01-01 00:00:04');
 
   beforeEach(() => {
-    redisLock = jasmine.createSpy('redisLock');
+    freeLock = jasmine.createSpy('freeLock');
+    redisLockMethod = jasmine.createSpy('redisLockMethod').and.callFake((lockName, cb) => cb(freeLock));
+    redisLock = jasmine.createSpy('redisLock').and.returnValue(redisLockMethod);
+
+    // redisClient.get = jasmine.createSpy('get').and.callFake((jobName, cb) => cb(null, 1480508046336));
+    redisClient.set = jasmine.createSpy('set').and.callFake((jobName, value, cb) => cb());
+
+    spyOn(later, 'setInterval').and.callFake(func => func());
+
     cronivo = proxyquire('../../src/cronivo', {
       'redis-lock': redisLock,
+      later,
     });
+
+    jasmine.clock().install();
+    jasmine.clock().mockDate(basetime);
+  });
+
+  afterEach(() => {
+    jasmine.clock().uninstall();
   });
 
   describe('setUp:', () => {
@@ -20,6 +42,59 @@ describe('cronivo:', () => {
       const result = cronivo(argument);
       expect(redisLock).toHaveBeenCalledWith(argument);
       expect(result.setInterval).toBeDefined();
+    });
+  });
+
+  describe('setInterval:', () => {
+    let setInterval;
+    let schedule;
+    let action;
+
+    beforeEach(() => {
+      setInterval = cronivo(redisClient).setInterval;
+      schedule = later.parse.recur().every(5).second();
+      action = jasmine.createSpy('action');
+    });
+
+    it('should call the function if no value was on the database', () => {
+      redisClient.get = jasmine.createSpy('get').and.callFake((jobName, cb) => cb(null, null));
+
+      const jobName = 'jobName';
+      setInterval(action, schedule, jobName);
+
+      expect(redisLockMethod).toHaveBeenCalledWith(`${jobName}Lock`, jasmine.any(Function));
+      expect(redisClient.get).toHaveBeenCalledWith(jobName, jasmine.any(Function));
+      expect(redisClient.set).toHaveBeenCalledWith(jobName, basetime.getTime() + 1000,
+        jasmine.any(Function));
+      expect(freeLock).toHaveBeenCalled();
+      expect(action).toHaveBeenCalled();
+    });
+
+    it('should call the function if the database value was earlier than now', () => {
+      redisClient.get = jasmine.createSpy('get').and.callFake((jobName, cb) => cb(null, basetime.getTime() - 4000));
+
+      const jobName = 'jobName';
+      setInterval(action, schedule, jobName);
+
+      expect(redisLockMethod).toHaveBeenCalledWith(`${jobName}Lock`, jasmine.any(Function));
+      expect(redisClient.get).toHaveBeenCalledWith(jobName, jasmine.any(Function));
+      expect(redisClient.set).toHaveBeenCalledWith(jobName, basetime.getTime() + 1000,
+        jasmine.any(Function));
+      expect(freeLock).toHaveBeenCalled();
+      expect(action).toHaveBeenCalled();
+    });
+
+    it('should not call the function if the database value is greater than now', () => {
+      redisClient.get = jasmine.createSpy('get').and.callFake((jobName, cb) => cb(null, basetime.getTime() + 1000));
+
+      const jobName = 'jobName';
+      setInterval(action, schedule, jobName);
+
+      expect(redisLockMethod).toHaveBeenCalledWith(`${jobName}Lock`, jasmine.any(Function));
+      expect(redisClient.get).toHaveBeenCalledWith(jobName, jasmine.any(Function));
+      expect(redisClient.set).not.toHaveBeenCalled();
+      expect(freeLock).toHaveBeenCalled();
+      expect(action).not.toHaveBeenCalled();
     });
   });
 });
--- src/cronivo.js
+++ src/cronivo.js
@@ -15,13 +15,13 @@ const api = {
       redisLock(`${jobName}Lock`, (done) => {
         // Gets the job next execution saved on redis
         redisClient.get(jobName, (err, reply) => {
-          // If the next execution is null or greater then the current next, then this job must
+          // If the next execution (saved on redis) is null or greater than now, then this job must
           // execute, else, it was already executed
-          const nextExecution = later.schedule(schedule).next(1);
-          // Sets milliseconds to zero since later doesn't use or set it
-          nextExecution.setMilliseconds(0);
-          if (_.isNil(reply) || reply.toString() < nextExecution) {
+          if (_.isNil(reply) || reply.toString() < new Date().getTime()) {
             // Sets the next execution time on redis so other jobs wont run
+            const nextExecution = later.schedule(schedule).next(1);
+            // Sets milliseconds to zero since later doesn't use or set it
+            nextExecution.setMilliseconds(0);
             redisClient.set(jobName, nextExecution.getTime(), () => {
               // Releases the lock since it is no longer required
               done();

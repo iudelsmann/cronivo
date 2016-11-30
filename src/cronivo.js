69a202156c9f7b5dd3ae09195ccc503d4c6eb205
Ivo Udelsmann <ivo.udelsmann@evoluservices.com>
2016-11-30 15:55:46 -0200

    Fallback to comparing next schedules instead of now
    - If calculating next execution just after current execution starts, Later returns the current execution, comparing to now broke the algorithm

--- src/cronivo.js
+++ src/cronivo.js
@@ -17,11 +17,11 @@ const api = {
         redisClient.get(jobName, (err, reply) => {
           // If the next execution (saved on redis) is null or greater than now, then this job must
           // execute, else, it was already executed
-          if (_.isNil(reply) || reply.toString() < new Date().getTime()) {
+          const nextExecution = later.schedule(schedule).next(1);
+          // Sets milliseconds to zero since later doesn't use or set it
+          nextExecution.setMilliseconds(0);
+          if (_.isNil(reply) || reply.toString() < nextExecution.getTime()) {
             // Sets the next execution time on redis so other jobs wont run
-            const nextExecution = later.schedule(schedule).next(1);
-            // Sets milliseconds to zero since later doesn't use or set it
-            nextExecution.setMilliseconds(0);
             redisClient.set(jobName, nextExecution.getTime(), () => {
               // Releases the lock since it is no longer required
               done();

cb05061b6d90be7a9fc2998b63b86ea253993b36
Ivo Udelsmann <iudelsmann@gmail.com>
2016-11-30 21:28:35 -0200

    Create setTimeout method
    - Create setTimeout for scheduling single execution
    - Add jsDocs on methods
    - Rename func to action

--- spec/tests/cronivo.test.js
+++ spec/tests/cronivo.test.js
@@ -21,6 +21,7 @@ describe('cronivo:', () => {
     redisClient.set = jasmine.createSpy('set').and.callFake((jobName, value, cb) => cb());
 
     spyOn(later, 'setInterval').and.callFake(func => func());
+    spyOn(later, 'setTimeout').and.callFake(func => func());
 
     cronivo = proxyquire('../../src/cronivo', {
       'redis-lock': redisLock,
@@ -61,6 +62,7 @@ describe('cronivo:', () => {
       const jobName = 'jobName';
       setInterval(action, schedule, jobName);
 
+      expect(later.setInterval).toHaveBeenCalled();
       expect(redisLockMethod).toHaveBeenCalledWith(`${jobName}Lock`, jasmine.any(Function));
       expect(redisClient.get).toHaveBeenCalledWith(jobName, jasmine.any(Function));
       expect(redisClient.set).toHaveBeenCalledWith(jobName, basetime.getTime() + 1000,
@@ -75,6 +77,7 @@ describe('cronivo:', () => {
       const jobName = 'jobName';
       setInterval(action, schedule, jobName);
 
+      expect(later.setInterval).toHaveBeenCalled();
       expect(redisLockMethod).toHaveBeenCalledWith(`${jobName}Lock`, jasmine.any(Function));
       expect(redisClient.get).toHaveBeenCalledWith(jobName, jasmine.any(Function));
       expect(redisClient.set).toHaveBeenCalledWith(jobName, basetime.getTime() + 1000,
@@ -89,6 +92,63 @@ describe('cronivo:', () => {
       const jobName = 'jobName';
       setInterval(action, schedule, jobName);
 
+      expect(later.setInterval).toHaveBeenCalled();
+      expect(redisLockMethod).toHaveBeenCalledWith(`${jobName}Lock`, jasmine.any(Function));
+      expect(redisClient.get).toHaveBeenCalledWith(jobName, jasmine.any(Function));
+      expect(redisClient.set).not.toHaveBeenCalled();
+      expect(freeLock).toHaveBeenCalled();
+      expect(action).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('setTimeout:', () => {
+    let setTimeout;
+    let schedule;
+    let action;
+
+    beforeEach(() => {
+      setTimeout = cronivo(redisClient).setTimeout;
+      schedule = later.parse.recur().every(5).second();
+      action = jasmine.createSpy('action');
+    });
+
+    it('should call the function if no value was on the database', () => {
+      redisClient.get = jasmine.createSpy('get').and.callFake((jobName, cb) => cb(null, null));
+
+      const jobName = 'jobName';
+      setTimeout(action, schedule, jobName);
+
+      expect(later.setTimeout).toHaveBeenCalled();
+      expect(redisLockMethod).toHaveBeenCalledWith(`${jobName}Lock`, jasmine.any(Function));
+      expect(redisClient.get).toHaveBeenCalledWith(jobName, jasmine.any(Function));
+      expect(redisClient.set).toHaveBeenCalledWith(jobName, basetime.getTime() + 1000,
+        jasmine.any(Function));
+      expect(freeLock).toHaveBeenCalled();
+      expect(action).toHaveBeenCalled();
+    });
+
+    it('should call the function if the database value was earlier than now', () => {
+      redisClient.get = jasmine.createSpy('get').and.callFake((jobName, cb) => cb(null, basetime.getTime() - 4000));
+
+      const jobName = 'jobName';
+      setTimeout(action, schedule, jobName);
+
+      expect(later.setTimeout).toHaveBeenCalled();
+      expect(redisLockMethod).toHaveBeenCalledWith(`${jobName}Lock`, jasmine.any(Function));
+      expect(redisClient.get).toHaveBeenCalledWith(jobName, jasmine.any(Function));
+      expect(redisClient.set).toHaveBeenCalledWith(jobName, basetime.getTime() + 1000,
+        jasmine.any(Function));
+      expect(freeLock).toHaveBeenCalled();
+      expect(action).toHaveBeenCalled();
+    });
+
+    it('should not call the function if the database value is greater than now', () => {
+      redisClient.get = jasmine.createSpy('get').and.callFake((jobName, cb) => cb(null, basetime.getTime() + 1000));
+
+      const jobName = 'jobName';
+      setTimeout(action, schedule, jobName);
+
+      expect(later.setTimeout).toHaveBeenCalled();
       expect(redisLockMethod).toHaveBeenCalledWith(`${jobName}Lock`, jasmine.any(Function));
       expect(redisClient.get).toHaveBeenCalledWith(jobName, jasmine.any(Function));
       expect(redisClient.set).not.toHaveBeenCalled();
--- src/cronivo.js
+++ src/cronivo.js
@@ -6,37 +6,78 @@ const later = require('later');
 let redisClient;
 let redisLock;
 
+/**
+ * Executes the action for either setInterval or setTimeout, using lock and redis to synchronize
+ * execution.
+ *
+ * @param  {Function}       action   The function to be executed on the provided schedule
+ * @param  {schedule}       schedule A later schedule for the action
+ * @param  {string}         jobName  The name of the job to be executed
+ * @private
+ */
+function executeAction(action, schedule, jobName) {
+  // Locks so other jobs wait before executing
+  redisLock(`${jobName}Lock`, (done) => {
+    // Gets the job next execution saved on redis
+    redisClient.get(jobName, (err, reply) => {
+      // If the next execution (saved on redis) is null or greater than now, then this job must
+      // execute, else, it was already executed
+      const nextExecution = later.schedule(schedule).next(1);
+      // Sets milliseconds to zero since later doesn't use or set it
+      nextExecution.setMilliseconds(0);
+      if (_.isNil(reply) || reply.toString() < nextExecution.getTime()) {
+        // Sets the next execution time on redis so other jobs wont run
+        redisClient.set(jobName, nextExecution.getTime(), () => {
+          // Releases the lock since it is no longer required
+          done();
+          // Calls the method passed by the user
+          action();
+        });
+      } else {
+        done();
+      }
+    });
+  });
+}
+
+// Public methods
 const api = {
-  setInterval(func, schedule, jobName) {
+  /**
+   * Executes the provided function repeatedely in the provided schedule. Uses the job name as a key
+   * on redis, therefore it must be unique.
+   *
+   * @param  {Function}       action   The function to be executed on the provided schedule
+   * @param  {schedule}       schedule A later schedule for the action
+   * @param  {string}         jobName  The name of the job to be executed
+   */
+  setInterval(action, schedule, jobName) {
     // Uses later setInterval method to schedule the job, with some extra instructions to ensure
     // synced execution
-    later.setInterval(() => {
-      // Locks so other jobs wait before executing
-      redisLock(`${jobName}Lock`, (done) => {
-        // Gets the job next execution saved on redis
-        redisClient.get(jobName, (err, reply) => {
-          // If the next execution (saved on redis) is null or greater than now, then this job must
-          // execute, else, it was already executed
-          const nextExecution = later.schedule(schedule).next(1);
-          // Sets milliseconds to zero since later doesn't use or set it
-          nextExecution.setMilliseconds(0);
-          if (_.isNil(reply) || reply.toString() < nextExecution.getTime()) {
-            // Sets the next execution time on redis so other jobs wont run
-            redisClient.set(jobName, nextExecution.getTime(), () => {
-              // Releases the lock since it is no longer required
-              done();
-              // Calls the method passed by the user
-              func();
-            });
-          } else {
-            done();
-          }
-        });
-      });
-    }, schedule);
+    later.setInterval(() => executeAction(action, schedule, jobName), schedule);
+  },
+
+  /**
+   * Executes the provided function once in the provided schedule. Uses the job name as a key on
+   * redis, therefore it must be unique.
+   *
+   * @param  {Function}       action   The function to be executed on the provided schedule
+   * @param  {schedule}       schedule A later schedule for the action
+   * @param  {string}         jobName  The name of the job to be executed
+   */
+  setTimeout(action, schedule, jobName) {
+    // Uses later setInterval method to schedule the job, with some extra instructions to ensure
+    // synced execution
+    later.setTimeout(() => executeAction(action, schedule, jobName), schedule);
   },
 };
 
+/**
+ * Sets up the module, saving the redis client and using it to startup redisLock.
+ *
+ * @param {RedisClient} client A redis client connected to a redis instance where the jobs data will
+ *                             be saved
+ * @return {Object}            The public methods available to schedule jobs
+ */
 function setUp(client) {
   redisClient = client;
   // Uses the provided client to configure redisLock

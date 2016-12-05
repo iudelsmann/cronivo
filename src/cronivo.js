dfe6155174ef5e155028ceba6618ac101ce5b218
Ivo Udelsmann <iudelsmann@gmail.com>
2016-12-04 22:47:22 -0200

    Use proper next execution time
    - Sometimes next(1) returns the current execution, set the next after that when it happens

--- src/cronivo.js
+++ src/cronivo.js
@@ -19,10 +19,17 @@ function executeAction(action, schedule, jobName) {
     this.redisClient.get(jobName, (err, reply) => {
       // If the next execution (saved on redis) is null or greater than now, then this job must
       // execute, else, it was already executed
-      const nextExecution = later.schedule(schedule).next(1);
+      const nextExecutions = later.schedule(schedule).next(2);
+
       // Sets milliseconds to zero since later doesn't use or set it
-      nextExecution.setMilliseconds(0);
-      if (_.isNil(reply) || reply.toString() < nextExecution.getTime()) {
+      _.forEach(nextExecutions, execution => execution.setMilliseconds(0));
+
+      const now = new Date().getTime();
+
+      const nextExecution = now > nextExecutions[0].getTime()
+        ? nextExecutions[1] : nextExecutions[0];
+
+      if (_.isNil(reply) || reply.toString() < now) {
         // Sets the next execution time on redis so other jobs wont run
         this.redisClient.set(jobName, nextExecution.getTime(), () => {
           // Releases the lock since it is no longer required
